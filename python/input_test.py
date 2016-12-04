705d350c545489c0c0a0297021ca2272c0967806
AlbertGithubHome <shihengzhen101@163.com>
2016-12-04 17:26:54 +0800

    learn input functon

---
+++ python/input_test.py
@@ -0,0 +1,10 @@
+print("please input your name：")
+name = input()
+print("Hello", name)
+
+
+age = input("please input your age：")
+print("Your age is", age)
+
+birthday=input("my birthday is:")
+print("my birthday is",birthday)
