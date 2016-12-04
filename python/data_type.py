55471e349808e9618ba0ec9ad98086a57e3c53fb
AlbertGithubHome <shihengzhen101@163.com>
2016-12-04 18:06:14 +0800

    learn data type

---
+++ python/data_type.py
@@ -0,0 +1,29 @@
+print("this is string")
+
+print(123)
+
+print(0.000012)
+
+print(False)
+
+print(5 > 3)
+
+print(None)
+
+# this is annotation format
+
+print("10 / 3 =", 10 / 3)
+
+print("10 // 3=", 10 // 3)
+
+# output result
+'''
+this is string
+123
+1.2e-05
+False
+True
+None
+10 / 3 = 3.3333333333333335
+10 // 3= 3
+'''
