767578aeaa1cd9b4dea288f10c0ec6269df4cab0
AlbertGithubHome <shihengzhen101@163.com>
2016-12-04 19:22:43 +0800

    learn format output

---
+++ python/format_test.py
@@ -0,0 +1,14 @@
+print('int num = %d' % 123)
+
+print('float num = %f' % .123)
+
+print('string = %s'% 'I am a boy')
+
+print('a number 33 = %x' % 33)
+
+
+print('%2d-%02d' % (3, 1))
+
+print('%.2f' % 3.1415926)
+
+print('Age: %s. Gender: %s' % (25, True))
