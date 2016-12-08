a951a93eaed11335d3f77c7b230e5d089ae3a4de
albert <alberts101@163.com>
2016-12-08 16:09:55 +0800

    learn python lambda

---
+++ python/lambda_test.py
@@ -0,0 +1,13 @@
+#lambda test
+
+p = lambda x: x * x
+print('p(10) =', p(10))
+print('p =', p)
+
+def func():
+    return lambda x, y: x + y
+
+myfunc = func()
+
+print('myfunc =', myfunc)
+print('myfunc() =', myfunc(4, 5))
