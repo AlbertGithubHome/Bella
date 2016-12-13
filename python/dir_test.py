de60ada67128d5b1d0806e1d3d50385e0d0a695f
albert <alberts101@163.com>
2016-12-13 19:50:43 +0800

    modify dir function example

--- python/dir_test.py
+++ python/dir_test.py
@@ -6,3 +6,20 @@ print('dir(S) =', dir(S))
 
 print('ABC'.__len__())
 print(len('ABC'))
+
+
+class MyDog(object):
+    def __len__(self):
+        return 100
+
+dog = MyDog()
+print('len(dog) =', len(dog))
+
+print(hasattr(dog, 'x'))
+
+setattr(dog, 'x', 10)
+print(hasattr(dog, 'x'))
+print(getattr(dog, 'x'))
+
+# the attr is not exist
+print(getattr(dog, 'y', 404))
