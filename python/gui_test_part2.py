d1840f052b6af38851080f17d3d6281446b1dd7f
albert <alberts101@163.com>
2016-12-14 13:39:37 +0800

    learn python GUI part2

---
+++ python/gui_test_part2.py
@@ -0,0 +1,27 @@
+# GUI test part 2
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
+
+app = Application()
+
+# set title for window
+app.master.title('Hello World')
+
+# main message loop
+app.mainloop()
