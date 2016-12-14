1b41b700c7ccf1e76758ed1bd8280499cbcb017c
albert <alberts101@163.com>
2016-12-14 13:38:49 +0800

    learn python GUI part1

---
+++ python/gui_test_part1.py
@@ -0,0 +1,22 @@
+# GUI test part 1
+from tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+app = Application()
+
+# set title for window
+app.master.title('Hello World')
+
+# main message loop
+app.mainloop()
