0da6ae916c0de734a4f11ebc986ea778f613a100
albert <alberts101@163.com>
2016-12-16 15:00:56 +0800

    learn python error

---
+++ python/error_test.py
@@ -0,0 +1,13 @@
+#Error test
+#
+try:
+    print('try...')
+    r = 10 / int('a')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+finally:
+    print('finally...')
+print('END')
