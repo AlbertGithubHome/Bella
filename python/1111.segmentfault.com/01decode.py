8d8c8ce225f338df2b10995893c0711ed2548fae
albert <alberts101@163.com>
2016-12-19 13:40:25 +0800

    reduce extra code for partical problem

--- python/1111.segmentfault.com/01decode.py
+++ python/1111.segmentfault.com/01decode.py
@@ -3,14 +3,12 @@ import base64
 from functools import reduce
 
 # read source file content
-file_handle = open('01string.txt', 'r')
-content = file_handle.read().replace('_', '1').split()
-file_handle.close()
+with open('01string.txt', 'r') as file_handle:
+    content = file_handle.read().replace('_', '1').split()
 
 # decode with base64
 decode_content = base64.b64decode(reduce(lambda x, y : x + y, map(lambda n : chr(int(n, base=2)), content)))
 
 # output result into a file of 'tar.gz' type
-file_handle = open('1111.segmentfault.com.tar.gz', 'wb')
-file_handle.write(decode_content)
-file_handle.close()
+with open('1111.segmentfault.com.tar.gz', 'wb') as file_handle:
+    file_handle.write(decode_content)
