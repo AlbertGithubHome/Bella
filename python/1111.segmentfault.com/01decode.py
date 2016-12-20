3b3191c5b6a8ee94e00ce91bbfe7be3cc70cda9e
albert <alberts101@163.com>
2016-12-20 17:50:47 +0800

    reduce code

--- python/1111.segmentfault.com/01decode.py
+++ python/1111.segmentfault.com/01decode.py
@@ -1,13 +1,12 @@
 # import necessary package
-import base64
-from functools import reduce
+import base64, functools
 
 # read source file content
 with open('01string.txt', 'r') as file_handle:
     content = file_handle.read().replace('_', '1').split()
 
 # decode with base64
-decode_content = base64.b64decode(reduce(lambda x, y : x + y, map(lambda n : chr(int(n, base=2)), content)))
+decode_content = base64.b64decode(functools.reduce(lambda x, y : x + y, map(lambda n : chr(int(n, base=2)), content)))
 
 # output result into a file of 'tar.gz' type
 with open('1111.segmentfault.com.tar.gz', 'wb') as file_handle:
