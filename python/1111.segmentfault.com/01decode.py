f9774ff6d8a18edead879939b5088071cb8a1b62
albert <alberts101@163.com>
2016-12-20 17:57:55 +0800

    keep reducing code

--- python/1111.segmentfault.com/01decode.py
+++ python/1111.segmentfault.com/01decode.py
@@ -5,9 +5,6 @@ import base64, functools
 with open('01string.txt', 'r') as file_handle:
     content = file_handle.read().replace('_', '1').split()
 
-# decode with base64
-decode_content = base64.b64decode(functools.reduce(lambda x, y : x + y, map(lambda n : chr(int(n, base=2)), content)))
-
-# output result into a file of 'tar.gz' type
+# decode with base64 and output result into a file of 'tar.gz' type
 with open('1111.segmentfault.com.tar.gz', 'wb') as file_handle:
-    file_handle.write(decode_content)
+    file_handle.write(base64.b64decode(functools.reduce(lambda x, y : x + y, map(lambda n : chr(int(n, base=2)), content))))
