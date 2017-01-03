5c2c959064fd4479777cefb4928b158c7b37fde5
albert <alberts101@163.com>
2017-01-03 09:58:36 +0800

    learn python coroutine

---
+++ python/coroutine_test.py
@@ -0,0 +1,24 @@
+# Coroutine test
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def producer(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return %s' % r)
+    c.close()
+
+c = consumer()
+producer(c)
