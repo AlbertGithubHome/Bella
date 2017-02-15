bc094fc07b371121345f3fdac61ccf2fa5e2e3e9
albert <alberts101@163.com>
2017-02-15 10:43:23 +0800

    update little lightning tools

--- python/find_title/titletools.py
+++ python/find_title/titletools.py
@@ -6,7 +6,7 @@ import urllib.request, re
 
 # configuration
 req_headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ' 'Chrome/51.0.2704.63 Safari/537.36'
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)' 'Chrome/51.0.2704.63 Safari/537.36'
 }
 
 re_template = r'(?<=target="_blank">)(.+?)(?=</a> </li>)'
