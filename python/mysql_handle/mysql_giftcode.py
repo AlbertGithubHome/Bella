ac40964e728c1200ff2bcbeac7f596421d3ed651
albert <alberts101@163.com>
2017-02-22 18:13:24 +0800

    add exec sql with binary data

---
+++ python/mysql_handle/mysql_giftcode.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import struct
+from io import BytesIO
+
+itemList = [
+    [1, 0, 3, 0, 10],
+    [1, 0, 4, 0, 20]
+]
+
+
+def pack_item(itemlist):
+    data = BytesIO()
+    for item in itemList:
+        result = struct.pack('IIIII', item[0], item[1], item[2], item[3], item[4])
+        data.write(result)
+    return data
+
+conn = mysql.connector.connect(user = 'root', password = 'jgy', database = 'rewardcode')
+cursor = conn.cursor()
+
+item_data = pack_item(itemList)
+cursor.execute('call AddGiftCode(%s, %s, %s, %s)', ['J123456789111', '3', 1587756977, item_data.getvalue()])
+conn.commit()
+cursor.close()
+conn.close()
+
