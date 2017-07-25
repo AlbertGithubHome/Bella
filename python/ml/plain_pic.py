4b464e1075ac0919b06c89cca17f0be493c40a19
albert <alberts101@163.com>
2017-07-25 19:58:03 +0800

    add a test for make plain pic

---
+++ python/ml/plain_pic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#a test for making plain picture
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def make_pic():
+    dates = pd.date_range('2017-07-22', periods = 6)
+    df = pd.DataFrame(np.random.randn(6,4), index = dates, columns = list('ABCD'))
+    plt.plot(df, "-", label='a')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    make_pic()
