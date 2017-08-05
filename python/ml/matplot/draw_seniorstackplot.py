02a8910775da528f2d1faad1810edd6b86c91451
albert <alberts101@163.com>
2017-08-05 10:49:12 +0800

    pyplot stack plot with legend

--- python/ml/matplot/draw_seniorstackplot.py
+++ python/ml/matplot/draw_seniorstackplot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#a test for drawing a stack plot
+#a test for drawing a senior stack plot
 
 __author__ = 'AlbertS'
 
@@ -12,6 +12,10 @@ sleeping = [10,8,6,11,10,10,12]
 eating =   [2,6,4,3,6,8,10]
 working =  [12,10,14,10,8,6,2]
 
+plt.plot([], [], color='m', label='Sleeping', linewidth=1)
+plt.plot([], [], color='b', label='Eating', linewidth=1)
+plt.plot([], [], color='r', label='Working', linewidth=1)
+
 plt.stackplot(days, sleeping, eating, working, colors=['m','b','r'])
 plt.xlabel('x')
 plt.ylabel('y')
@@ -19,5 +23,7 @@ plt.ylabel('y')
 plt.title('Stack Plot')
 plt.title('Stack Plot')
 plt.title('Stackyy Plot')
+
+plt.legend()
 plt.show()
 
