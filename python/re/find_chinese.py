cb6c8b0bc8e4dec624f5e220df9c4d1ac1dcca42
albert <alberts101@163.com>
2017-09-28 14:29:46 +0800

    add find chinese tools

---
+++ python/re/find_chinese.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'AlbertS'
+
+# find the line of containing chinese in files
+import re
+from io import BytesIO
+
+def start_find_chinese():
+
+    find_count = 0;
+    with open('strkey_ko_untranslate.txt', 'wb') as outfile:
+        with open('strkey_ko.txt', 'rb') as infile:
+            while True:
+                content = infile.readline()
+                if re.match(r'(.*[\u4E00-\u9FA5]+)|([\u4E00-\u9FA5]+.*)', content.decode('utf-8')):
+                    outfile.write(content)
+                    find_count += 1;
+
+                if not content:
+                    return find_count
+
+# start to crawler
+if __name__ == '__main__':
+    count = start_find_chinese()
+    print("find complete! count =", count)
---
+++ python/re/findchinese.bat
@@ -0,0 +1,1 @@
+python find_chinese.py
