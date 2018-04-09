e47c1d97c00f5718b29b23318f369bbc80a95e10
albert <alberts101@163.com>
2018-04-09 20:03:17 +0800

    add python lib about image

--- python/albert_learn_lib.py
+++ python/albert_learn_lib.py
@@ -23,3 +23,12 @@ from aip import AipOcr
 # 同样是用于识别图片文字的，来源于百度的api。
 # 如果已安装pip，执行pip install baidu-aip即可；如果已安装setuptools，执行python setup.py install即可。
 # 第一次使用是在2018-03-28 20:48:36，用于和pytesseract识别图片做对比
+
+from PIL import Image
+# 一开始是在廖雪峰老师的Python教程上学到的，主要是用来处理图片的，可以做旋转、模糊二值化等等。
+# PIL：Python Imaging Library，已经是Python平台事实上的图像处理标准库了。PIL功能非常强大，但API却非常简单易用。
+# 由于PIL仅支持到Python 2.7，加上年久失修，于是一群志愿者在PIL的基础上创建了兼容的版本，名字叫Pillow，
+# Pillow支持最新Python 3.x，又加入了许多新特性，因此，我们可以直接安装使用Pillow。
+# Python 2.7安装PIL，在Debian/Ubuntu Linux下直接通过apt安装：$ sudo apt-get install python-imaging。
+# Python 2.7安装PIL，Windows平台就去PIL官方网站下载exe安装包。[url](http://pythonware.com/products/pil/)
+# Python 3.x安装PIL,如果安装了Anaconda，Pillow就已经可用了。否则，需要在命令行下通过pip安装：$ pip install pillow
