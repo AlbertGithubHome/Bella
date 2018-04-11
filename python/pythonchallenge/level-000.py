b8d36563981d9795d8a11aa2de16bd1fab653d25
albert <alberts101@163.com>
2018-04-11 20:57:11 +0800

    add solution for level 0 of python challenge

---
+++ python/pythonchallenge/level-000.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date     : 2018-04-11 20:50:13
+# @Author   : Albert Shi
+# @Link     : http://blog.csdn.net/albertsh
+# @Github   : https://github.com/AlbertGithubHome
+__author__ = 'AlbertS'
+# @Subject  : python challenge
+# 
+# 思路：直接计算2^38的结果填入浏览器地址栏即可
+#       python计算大数据还是很方便的，对于其他很多语言来说这个结果已经超出了表示范围
+#       
+#       
+# 备注：1. python challenge home page : http://www.pythonchallenge.com/
+#       2. current level url : http://www.pythonchallenge.com/pc/def/0.html
+#       3. next level url : http://www.pythonchallenge.com/pc/def/274877906944.html
+
+print(2**38)
+
+# 运行结果
+'''
+274877906944
+[Finished in 0.1s]
+'''
