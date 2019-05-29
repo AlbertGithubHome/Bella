7438847e2b4f2406ba0c52022397a73966e347dc
AlbertGithubHome <shihengzhen101@163.com>
2019-05-30 00:27:17 +0800

    modify drawing curve

--- python/showcode/drawcurve.py
+++ python/showcode/drawcurve.py
@@ -13,27 +13,27 @@ import matplotlib.pyplot as plt
 x = np.arange(-np.pi, np.pi, 0.1)
 y = np.sin(x)
 
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('line')
+
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-plt.title("正弦波-余弦波")
-#plt.xlabel("xxxxxx", 'position', 1)
-h = plt.ylabel("yyyyyy", labelpad = 20, ha='left', va="top")
-print(h)
-plt.text(0, 1, 'newyyyyyy')
 
-#plt.figure(figsize=(8,6), dpi=100)
+#plt.xlabel('x轴')
+#plt.ylabel('y轴')
+plt.text(3.2, 0.1, 'x轴')
+plt.text(0.1, 1.0, 'y轴')
 
-# 创建一个新的 1 * 1 的子图，接下来的图样绘制在其中的第 1 块
-ax = plt.gca()#plt.subplot(1, 1, 1)
-print(ax)
+plt.title('正弦波')
+
+ax = plt.gca()
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
-ax.xaxis.set_ticks_position('bottom')
 ax.spines['bottom'].set_position(('data',0))
-ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data',0))
 
-plt.plot(x, y)
+plt.plot(x, y, ":g")
 plt.show()
 
 
