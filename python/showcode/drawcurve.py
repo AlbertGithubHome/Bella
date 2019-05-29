a5195738a712c15de939e0e8099eefb06dfb3570
AlbertS <shihengzhen101@163.com>
2019-05-29 18:12:01 +0800

    add example about complex curve

--- python/showcode/drawcurve.py
+++ python/showcode/drawcurve.py
@@ -16,13 +16,16 @@ y = np.sin(x)
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 plt.title("正弦波-余弦波")
-plt.xlabel("x 轴", horizontalalignment='right', verticalalignment="center")
-plt.ylabel("y 轴", verticalalignment="top")
+#plt.xlabel("xxxxxx", 'position', 1)
+h = plt.ylabel("yyyyyy", labelpad = 20, ha='left', va="top")
+print(h)
+plt.text(0, 1, 'newyyyyyy')
 
 #plt.figure(figsize=(8,6), dpi=100)
 
 # 创建一个新的 1 * 1 的子图，接下来的图样绘制在其中的第 1 块
 ax = plt.gca()#plt.subplot(1, 1, 1)
+print(ax)
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
