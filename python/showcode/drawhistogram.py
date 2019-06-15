b33cc2d96224f1fbed373065fabf1046914d5360
AlbertS <shihengzhen101@163.com>
2019-06-15 13:12:36 +0800

    add new pic about histogram

--- python/showcode/drawhistogram.py
+++ python/showcode/drawhistogram.py
@@ -5,24 +5,35 @@
 # @Link     : http://blog.csdn.net/albertsh
 # @Github   : https://github.com/AlbertGithubHome
 __author__ = 'AlbertS'
-# @Subject  : draw a histogram bar
+# @Subject  : draw a histogram
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-plt.title("直方图")
+plt.title("分数分布直方图")
 plt.xlabel("分数段")
 plt.ylabel("人数")
 
-a = np.array([22,87,5,43,56,73,55,54,11,20,51,5,79,31,27])
-hist, bins = np.histogram(a, bins=[0,20,40,60,80,100])
-print(hist)
-print(bins)
+# 分数
+score = np.array([87,72,63,56,73,55,54,79,31,27,96,11,90,51,75])
+# 分数段
+section = [0,20,40,60,80,100]
 
-plt.hist(a, bins=[0,60,80,100])#bins=[x for x in range(0, 100, 10)])
+# plt.hist(score, bins=section)#bins=[x for x in range(0, 100, 10)])
+# plt.show()
+
+plt.hist(score, bins=section, edgecolor='b')#bins=[x for x in range(0, 100, 10)])
+
+hist, bins = np.histogram(score, bins=section)
+for a,b in zip(section, hist):
+    plt.text(a+10, b+0.1, str(b), ha='center', va= 'bottom', fontsize=20)
+
+plt.ylim(0, max(hist) + 1)
 plt.show()
+
+
 
 
 
