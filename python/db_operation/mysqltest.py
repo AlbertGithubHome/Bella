53bf3cce09c28c829e4db8abfef5c9b241e196e3
AlbertS <shihengzhen101@163.com>
2019-08-01 15:27:07 +0800

    add new comments for project

---
+++ python/db_operation/mysqltest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date     : 2019-07-15 17:35:20
+# @Author   : Albert Shi
+# @Link     : http://blog.csdn.net/albertsh
+# @Github   : https://github.com/AlbertGithubHome
+__author__ = 'AlbertS'
+# @Subject  : a demo for using
+#
+
+import MySQLdb
+
