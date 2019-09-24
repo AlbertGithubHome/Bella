5696d32b7d3b3758fb093d224654d89bce04e159
AlbertS <shihengzhen101@163.com>
2019-09-24 20:17:17 +0800

    stock start

---
+++ python/stock/epoch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date     : 2019-9-24 20:13:26
+# @Author   : Albert Shi
+# @Link     : http://blog.csdn.net/albertsh
+# @Github   : https://github.com/AlbertGithubHome
+__author__ = 'AlbertS'
+# @Subject  : draw a picture about stock
+
+from matplotlib import pyplot as plt
+import matplotlib.finance as mpf
+from matplotlib.pylab import date2num
+import pandas as pd
+import datetime
+
+
+# 运行结果
+'''
+
+'''
