201db9e7bfbe9762d5d41274a4cef1ec9feef56a
AlbertGithubHome <shihengzhen101@163.com>
2019-10-02 21:28:20 +0800

    enrich candle

--- python/stock/previousV/advanced/detail_candle.py
+++ python/stock/previousV/advanced/detail_candle.py
@@ -46,53 +46,104 @@ def get_relation(pre_data, cur_data):
     else:
         return relation_type.NONE
 
-def analyze_tendency(low_list, high_list):
+def analyze_tendency(low_list, high_list, xlist, ylist):
     pre_relation = relation_type.UP
     pre_data = (low_list[0], high_list[0])
-    xlist = [0]
-    ylist = [low_list[0] / 2 + high_list[0]/ 2]
+    next_data = (0, 0)
+    start_pos = (low_list[0] / 2 + high_list[0] / 2)
+    pre_n = 0
+    next_n = 0
 
     item_count = len(low_list)
     for n in range(item_count):
         ret_relation = get_relation(pre_data, None if n + 1== item_count else (low_list[n+1], high_list[n+1]))
+        contain_relation = relation_type.NONE
         #print("real--------------->", ret_relation)
         if ret_relation == relation_type.NONE:
             break
         elif ret_relation == relation_type.UP or ret_relation == relation_type.DOWN:
             next_data = (low_list[n+1], high_list[n+1])
+            next_n = n+1
 
         elif ret_relation == relation_type.OPEN:
             if pre_relation == relation_type.UP:
                 next_data = (low_list[n], high_list[n+1])
+                next_n = n+1
             else:
                 next_data = (low_list[n+1], high_list[n])
+                next_n = n+1
+            contain_relation = ret_relation
             ret_relation = pre_relation
         elif ret_relation == relation_type.CLOSE:
             if pre_relation ==  relation_type.UP:
                 next_data = (low_list[n+1], high_list[n])
+                next_n = n
             else:
                 next_data = (low_list[n], high_list[n+1])
+                next_n = n
+            contain_relation = ret_relation
             ret_relation = pre_relation
 
+        #print(n, ret_relation, contain_relation)
+
         if ret_relation != pre_relation:
+            if n == 0:
+                xlist.append(n)
+                ylist.append(start_pos)
+            else:
+                xlist.append(pre_n)
+                ylist.append(pre_data[1] if pre_relation == relation_type.UP else pre_data[0])
+        elif n == 0:
             xlist.append(n)
-            ylist.append(pre_data[1] if pre_relation == relation_type.UP else pre_data[0])
+            ylist.append(start_pos)
 
         pre_data = next_data
         pre_relation = ret_relation
+        pre_n = next_n
         #print(pre_relation, n)
 
     xlist.append(item_count - 1)
     ylist.append(high_list[item_count - 1] if pre_relation == relation_type.UP else low_list[item_count - 1])
 
-    plt.plot(xlist, ylist, color='r')
+    # plt.plot(xlist, ylist, color='y')
     print(xlist)
     print(ylist)
     print(item_count)
 
 def update_stock_data():
-    df = ts.get_hist_data(stock_code, ktype='5')
+    df = ts.get_hist_data(stock_code, ktype='30')
     df.to_csv(stock_code+'.csv', columns=['open','high','low','close'])
+
+def smooth_guides(xlist, ylist):
+    listlen = len(xlist)
+    retlistx = []
+    retlisty = []
+
+    if listlen > 0:
+        retlistx.append(xlist[0])
+        retlisty.append(ylist[0])
+        prex = xlist[0]
+
+    n = 1
+    while n < listlen:
+        if n == listlen - 1:
+            retlistx.append(xlist[n])
+            retlisty.append(ylist[n])
+            prex = xlist[n]
+        # elif xlist[n] - xlist[n - 1] >= 6:
+        #     retlistx.append(xlist[n])
+        #     retlisty.append(ylist[n])
+        #     prex = xlist[n]
+        elif xlist[n+1] - xlist[n] < 3:
+            n += 1
+        # elif xlist[n] - prex <= 2:
+        #     n += 1
+        else:
+            retlistx.append(xlist[n])
+            retlisty.append(ylist[n])
+            prex = xlist[n]
+        n += 1
+    return retlistx, retlisty
 
 def draw_candle():
     csv_data = pd.read_csv(stock_code+'.csv', low_memory = False) #防止弹出警告
@@ -110,7 +161,8 @@ def draw_candle():
 
     fig, ax = plt.subplots(figsize=(800/72, 450/72))
     fig.subplots_adjust(bottom=0.1)
-    mpf.candlestick_ohlc(ax, interval_quotes, colordown='#F5F5F5', colorup='#DCDCDC', width=0.2, alpha=1)
+    #mpf.candlestick_ohlc(ax, interval_quotes, colordown='#F5F5F5', colorup='#DCDCDC', width=0.2, alpha=1)
+    mpf.candlestick_ohlc(ax, interval_quotes, colordown='#53c156', colorup='#ff1717', width=0.2, alpha=1)
 
     # #https://matplotlib.org/examples/pylab_examples/date_index_formatter.html
     class MyFormatter(Formatter):
@@ -131,8 +183,16 @@ def draw_candle():
         #label.set_rotation(45)
         label.set_horizontalalignment('center')
 
-    analyze_tendency(low_quotes, high_quotes);
-    #plt.plot([0,10,15], [31.25,31.6,31.3], color='r')
+    # get point data
+    xlist = []
+    ylist = []
+    analyze_tendency(low_quotes, high_quotes, xlist, ylist);
+
+    xlist, ylist = smooth_guides(xlist, ylist);
+    print(xlist)
+    print(ylist)
+
+    plt.plot(xlist, ylist, color='y')
     plt.show()
 
 
