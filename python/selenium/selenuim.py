155da71f7f8f206dd0c54a3c0f071e3384fdef00
AlbertS <shihengzhen101@163.com>
2019-11-21 14:34:43 +0800

    add test for visit csdn web with selenium

--- python/selenium/selenuim.py
+++ python/selenium/selenuim.py
@@ -14,6 +14,28 @@ browser = webdriver.Firefox()
 
 browser.get("https://blog.csdn.net/albertsh/article/details/52231311")
 time.sleep(10)
-for i in range(1, 5):
-    browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-    time.sleep(1)
+browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+# time.sleep(10)
+# for i in range(1, 5):
+#     browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+#     time.sleep(1)
+
+#browser.get("https://blog.csdn.net/albertsh/article/details/102938754")
+
+# ele_id=browser.find_element_by_id('fanBox')
+# print(type(ele_id))
+# print(ele_id.text)
+# print(ele_id.get_attribute('textContent'))
+# print(ele_id.get_attribute('innerText'))
+# print(ele_id.get_attribute('innerHTML'))
+
+def get_csdn_pv(browser_obj):
+    try:
+        dl_list = browser.find_elements_by_class_name('text-center')
+        for element in dl_list:
+            if '访问' in element.text:
+                return element.get_attribute('title')
+    except:
+        return 0
+
+print(get_csdn_pv(browser))
---
+++ python/selenium/visit_csdn.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date     : 2019-11-20 21:00:54
+# @Author   : Albert Shi
+# @Link     : http://blog.csdn.net/albertsh
+# @Github   : https://github.com/AlbertGithubHome
+__author__ = 'AlbertS'
+# @Subject  : use selenium to visit csdn and collect article list
+
+import time
+import random
+from selenium import webdriver
+
+article_list_main_url='https://blog.csdn.net/albertsh/article/list/{0}?t=1&'
+article_detail_main_url='https://blog.csdn.net/albertsh/article/details/{0}\n'
+
+def get_csdn_pv(browser_obj):
+    try:
+        dl_list = browser.find_elements_by_class_name('text-center')
+        for element in dl_list:
+            if '访问' in element.text:
+                return element.get_attribute('title')
+    except:
+        return 0
+
+
+def collect_article_url_list(max_page):
+    try:
+        with open('articlelist.txt', 'w', encoding='utf-8') as outfile:
+            browser = webdriver.Firefox()
+            for n in range(max_page):
+                browser.get(article_list_main_url.format(n+1))
+                time.sleep(random.uniform(6.18, 8.26)) # wait page open
+
+                div_list = browser.find_elements_by_class_name('article-item-box')
+                print(div_list)
+                for element in div_list:
+                    entrance = element.get_attribute('data-articleid')
+                    outfile.write(article_detail_main_url.format(entrance))
+    except Exception as e:
+        raise e
+        return False
+    else:
+        return True
+
+if __name__ == '__main__':
+    print(collect_article_url_list(10))
