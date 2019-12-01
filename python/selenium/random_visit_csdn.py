cc2646828ff1db591a0015de6e5ef2b3600bddec
AlbertGithubHome <shihengzhen101@163.com>
2019-12-01 15:25:36 +0800

    add new file about proxy

--- python/selenium/random_visit_csdn.py
+++ python/selenium/random_visit_csdn.py
@@ -17,8 +17,6 @@ def get_csdn_pv(browser_obj):
     try:
         dl_list = browser_obj.find_elements_by_class_name('text-center')
         for element in dl_list:
-            # print(element.text)
-            # print(element.get_attribute('title'))
             if '访问' in element.text:
                 return element.get_attribute('title')
     except:
@@ -27,13 +25,14 @@ def get_csdn_pv(browser_obj):
 
 def random_visit_article(max_count):
     try:
-        url_list = get_article_url_list();
+        url_list = get_article_url_list()
         browser = webdriver.Firefox()
         for x in range(max_count):
+            #browser.delete_all_cookies()
             target_url = random.choice(url_list)
             browser.get(target_url)
             time.sleep(random.uniform(6.18, 50.26))
-            print(target_url, get_csdn_pv(browser))
+            print(target_url, get_csdn_pv(browser), x)
         browser.quit()
     except Exception as e:
         raise e
@@ -45,4 +44,4 @@ def get_article_url_list():
     return pandas.read_csv('articlelist.txt', header=None)[0].values.tolist();
 
 if __name__ == '__main__':
-    print(random_visit_article(100))
+    print(random_visit_article(100), "completed")
