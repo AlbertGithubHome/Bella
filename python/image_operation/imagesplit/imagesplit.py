9dadd8aed4e06c16a5a06298aa2fd1067a8a892f
AlbertS <shihengzhen101@163.com>
2020-02-04 14:08:45 +0800

    add row and col split part for tools

--- python/image_operation/imagesplit/imagesplit.py
+++ python/image_operation/imagesplit/imagesplit.py
@@ -26,7 +26,7 @@ def imsplit(input_file_name, row_part, col_part):
     for r in range(row_part):
         for c in range(col_part):
             end_width_pixel = (c + 1) * avg_width_pixel if c < col_part - 1 else w
-            end_height_pixel = (r + 1) * avg_height_pixel if c < row_part - 1 else h
+            end_height_pixel = (r + 1) * avg_height_pixel if r < row_part - 1 else h
             box = (c * avg_width_pixel, r * avg_height_pixel, end_width_pixel, end_height_pixel)
             output_file = os.path.join(file_path, file_name + '_' + str(r) + '_' + str(c) + ext)
             print(output_file)
@@ -55,9 +55,18 @@ class App(tk.Frame):
         self.pack()
 
         self.path=tk.StringVar()
+        self.row_part = tk.StringVar(value='3')
+        self.col_part = tk.StringVar(value='1')
         tk.Entry(self,state='readonly',text=self.path).grid(row=0,column=0,columnspan=2,sticky=tk.E+tk.W)
-        tk.Button(self,text='选择图片',command=self.choose_pic).grid(row=1,column=0)
-        tk.Button(self,text='进行切割',command=self.img_split).grid(row=1,column=1)
+        tk.Button(self,text='选择图片',command=self.choose_pic).grid(row=0,column=3,sticky=tk.E+tk.W)
+
+        tk.Label(self, text='切割行数：').grid(row=1,column=0)
+        self.entry_row=tk.Entry(self,text=self.path, textvariable=self.row_part)
+        self.entry_row.grid(row=1,column=1)
+        tk.Label(self, text='切割列数：').grid(row=1,column=2)
+        self.entry_col=tk.Entry(self,text=self.path, textvariable=self.col_part)
+        self.entry_col.grid(row=1,column=3)
+        tk.Button(self,text='进行切割',command=self.img_split).grid(row=1,column=4)
 
     def choose_pic(self):
         self.img_path=askopenfilename()
@@ -81,11 +90,17 @@ class App(tk.Frame):
         src_image = Image.open(self.img_path)
         resized_image = self.resize(src_image);
         self.tk_image = ImageTk.PhotoImage(image=resized_image)
-        self.label = tk.Label(self, image=self.tk_image)
-        self.label.grid(row=2,column=0,columnspan=2)
+        tk.Label(self, image=self.tk_image).grid(row=2,column=0,columnspan=2)
 
     def img_split(self):
-        imsplit(self.img_path, 3, 1)
+        self.row_part = self.entry_row.get();
+        self.col_part = self.entry_col.get();
+        print(self.row_part)
+        try:
+            imsplit(self.img_path, int(self.row_part), int(self.col_part))
+        except:
+            self.path.set('please select a image!')
+
 
 def mainApp(w, h):
     root = tk.Tk()
