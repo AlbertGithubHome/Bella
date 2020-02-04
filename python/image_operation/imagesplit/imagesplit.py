ff4c4aa6e17520c7bbf8abed161dba47ec538a30
AlbertS <shihengzhen101@163.com>
2020-02-04 15:34:26 +0800

    add guides line for spilt image tools

--- python/image_operation/imagesplit/imagesplit.py
+++ python/image_operation/imagesplit/imagesplit.py
@@ -9,7 +9,7 @@ __author__ = 'AlbertS'
 
 import os
 import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 from tkinter.filedialog import askopenfilename
 
 def imsplit(input_file_name, row_part, col_part):
@@ -44,7 +44,6 @@ def main():
     # root.iconbitmap('spider_128px_1169260_easyicon.net.ico')
     root.mainloop()
 
-
 main_w = 800
 main_h = 600
 class App(tk.Frame):
@@ -57,16 +56,36 @@ class App(tk.Frame):
         self.path=tk.StringVar()
         self.row_part = tk.StringVar(value='3')
         self.col_part = tk.StringVar(value='1')
-        tk.Entry(self,state='readonly',text=self.path).grid(row=0,column=0,columnspan=2)#,sticky=tk.E+tk.W)
-        tk.Button(self,text='选择图片',command=self.choose_pic).grid(row=0,column=3)
+        tk.Entry(self,state='readonly',text=self.path,width=90).grid(row=0,column=0,columnspan=4)#,sticky=tk.E+tk.W)
+        tk.Button(self,text='选择图片',command=self.choose_pic).grid(row=0,column=4,sticky=tk.W)
 
-        tk.Label(self, text='切割行数：').grid(row=1,column=0)
-        self.entry_row=tk.Entry(self,text=self.path, textvariable=self.row_part)
-        self.entry_row.grid(row=1,column=1)
-        tk.Label(self, text='切割列数：').grid(row=1,column=2)
-        self.entry_col=tk.Entry(self,text=self.path, textvariable=self.col_part)
-        self.entry_col.grid(row=1,column=3)
-        tk.Button(self,text='进行切割',command=self.img_split).grid(row=1,column=4)
+        spilt_num_changed = self.register(self.spilt_num_changed)
+        tk.Label(self, text='切割行数：').grid(row=1,column=0,sticky=tk.E)
+        self.entry_row=tk.Entry(self,text=self.path, textvariable=self.row_part,
+              validate='focusout',
+              validatecommand=(spilt_num_changed, '%P', '%v', '%W')
+              # %P表示 当输入框的值允许改变，该值有效。该值为当前文本框内容
+              # %v(小写大写不一样的)，当前validate的值
+              # %W表示该组件的名字)
+              )#,width=10)
+
+        self.entry_row.grid(row=1,column=1,sticky=tk.W)
+        tk.Label(self, text='切割列数：').grid(row=1,column=2,sticky=tk.E)
+        self.entry_col=tk.Entry(self,text=self.path, textvariable=self.col_part,
+              validate='focusout',
+              validatecommand=(spilt_num_changed, '%P', '%v', '%W')
+              )
+        self.entry_col.grid(row=1,column=3,sticky=tk.W)
+        tk.Button(self,text='进行切割',command=self.img_split).grid(row=1,column=4,sticky=tk.W)
+
+    def spilt_num_changed(self, content, reason, name):
+        # 光标失去时，这个函数就会执行
+        #print(content, reason, name)
+        try:
+            self.show_image();
+        except:
+            pass
+        return True
 
     def choose_pic(self):
         self.img_path=askopenfilename()
@@ -76,21 +95,40 @@ class App(tk.Frame):
         except:
             self.path.set('please select a image!')
 
-    def resize(self, pil_image):
+    def resize_and_drawline(self, pil_image):
         w, h = pil_image.size
-        f1 = 1.0*self.main_w/w
-        f2 = 1.0*self.main_h/h
+        padding = 20
+        f1 = (1.0*self.main_w-padding)/w
+        f2 = (1.0*self.main_h-padding)/h
         factor = min([f1, f2])
         width = int(w*factor)
         height = int(h*factor)
-        print(factor)
-        return pil_image.resize((width, height), Image.ANTIALIAS)
+        #print(factor)
+        pil_image=pil_image.resize((width, height), Image.ANTIALIAS)
+
+        self.row_part = self.entry_row.get();
+        self.col_part = self.entry_col.get();
+        row_part = int(self.row_part)
+        col_part = int(self.col_part)
+
+        avg_width_pixel = width // col_part
+        avg_height_pixel = height // row_part
+        pil_image = pil_image.convert('RGB')
+        draw =ImageDraw.Draw(pil_image)
+
+        for r in range(row_part):
+            draw.line((0,(r+1)*avg_height_pixel) + (width,(r+1)*avg_height_pixel), fill='red')
+        for c in range(col_part):
+            draw.line(((c+1)*avg_width_pixel,0) + ((c+1)*avg_width_pixel,height), fill='red')
+
+        #draw.line((0,0) + (width,height), fill='red')
+        return pil_image
 
     def show_image(self):
         src_image = Image.open(self.img_path)
-        resized_image = self.resize(src_image);
+        resized_image = self.resize_and_drawline(src_image);
         self.tk_image = ImageTk.PhotoImage(image=resized_image)
-        tk.Label(self, image=self.tk_image).grid(row=2,column=0,columnspan=2)
+        tk.Label(self, image=self.tk_image).grid(row=2,column=0,columnspan=5,ipady=15)
 
     def img_split(self):
         self.row_part = self.entry_row.get();
@@ -105,12 +143,12 @@ class App(tk.Frame):
 def mainApp(w, h):
     root = tk.Tk()
     root.geometry('{0}x{1}+400+200'.format(w, h))
-    #root.resizable(0,0)
-    root.title('图片切割程序')
+    root.resizable(0,0)
+    root.title('图片切割器')
     #root.overrideredirect(1)
     root.iconbitmap('icon.ico')
     app = App(root)
-    root.mainloop()
+    app.mainloop()
 
 if __name__ == '__main__':
     # imsplit('./20200204080200.jpg', 3, 1);
