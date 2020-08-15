8bd5052bfa381c410b8836660c6208d986eb48b0
AlbertGithubHome <shihengzhen101@163.com>
2020-08-15 15:59:30 +0800

    first package

---
+++ go/src/exercise/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"exercise/pkg"
+)
+
+func main() {
+	fmt.Println("go exercise.")
+	pkg.Fibonacci(10)
+}
---
+++ go/src/exercise/pkg/fibonacci.go
@@ -0,0 +1,17 @@
+package pkg
+
+import "fmt"
+
+/*
+Fibonacci : print fibonacci sequence 
+*/
+func Fibonacci(maxCount int) {
+
+	if maxCount <= 0 { return }
+
+	first, second := 0, 1
+	for i := 1; i <= maxCount; i++ {
+		first, second = second, first + second
+		fmt.Printf("%d ", first)
+	}
+}
