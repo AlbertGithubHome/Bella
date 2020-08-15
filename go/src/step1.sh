65df1aa6c51560bef864d95a4043cc3b1fbd01d8
AlbertGithubHome <shihengzhen101@163.com>
2020-08-15 12:39:16 +0800

    define go directory

---
+++ go/src/step1.sh
@@ -0,0 +1,38 @@
+#mkdir -p github.com
+cd github.com
+mkdir acroca cweill derekparker go-delve josharian karrick mdempsky pkg ramya-rao-a rogpeppe sqs uudashr
+cd acroca
+git clone https://github.com/acroca/go-symbols.git
+cd ..
+cd cweill
+git clone https://github.com/cweill/gotests.git
+cd ..
+cd derekparker
+git clone https://github.com/derekparker/delve.git
+cd ..
+cd go-delve
+git clone https://github.com/go-delve\delve.git
+cd ..
+cd josharian
+git clone https://github.com/josharian/impl.git
+cd ..
+cd karrick
+git clone https://github.com/karrick/godirwalk.git
+cd ..
+cd mdempsky
+git clone https://github.com/mdempsky/gocode.git
+cd ..
+cd pkg
+git clone https://github.com/pkg/errors.git
+cd ..
+cd ramya-rao-a
+git clone https://github.com/ramya-rao-a/go-outline.git
+cd ..
+cd rogpeppe
+git clone https://github.com/rogpeppe/godef.git
+cd ..
+cd sqs
+git clone https://github.com/sqs/goreturns.git
+cd ..
+cd uudashr
+git clone https://github.com/uudashr/gopkgs.git
---
+++ go/src/step2.sh
@@ -0,0 +1,4 @@
+# mkdir -p golang.org/x
+cd golang.org/x
+git clone https://github.com/golang/tools.git
+git clone https://github.com/golang/lint.git
